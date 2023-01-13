e711ccf338754b2532265d67d48b813837a8d491
Husain Bismil <husain@upfrontideas.net>
2023-01-13 00:57:23 +0000

    added styling

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -2,10 +2,20 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>- NCNews -</h1>
-      <Link to="/">Home</Link> | <Link to="/articles">View All Articles</Link>
-      <div className="spacer"></div>
+    <div id="header-wrap">
+      <div id="header">
+        <div id="header-inner" className="container-wrap">
+          <div id="header-logo-wrap">
+            <span>- NCNews -</span>
+          </div>
+
+          <div id="header-desktop-navmenu-wrap">
+            <Link to="/">Home</Link> |{" "}
+            <Link to="/articles">View All Articles</Link>
+          </div>
+        </div>
+      </div>
+      <div id="header-shadow"></div>
     </div>
   );
 };
